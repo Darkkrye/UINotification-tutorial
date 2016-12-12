3aef9bb085e951193ba7c3ed6897bd4079aed943
Openfield Mobility <pboudon@opnf.fr>
2016-12-12 16:48:11 +0100

    * Added complete scheduling notification system.

--- NotificationsUI/AppDelegate.swift
+++ NotificationsUI/AppDelegate.swift
@@ -9,6 +9,9 @@
 import UIKit
 import UserNotifications
 
+// https://www.appcoda.com/ios10-user-notifications-guide/
+// https://blog.pusher.com/how-to-send-ios-10-notifications-using-the-push-notifications-api/
+
 @UIApplicationMain class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
@@ -20,13 +23,14 @@ import UserNotifications
         })
         
         let action = UNNotificationAction(identifier: "remindLater", title: "Remind me later", options: [])
-        let category = UNNotificationCategory(identifier: "myCategory", actions: [action], intentIdentifiers: [], options: [])
+        let act = UNNotificationAction(identifier: "schedule", title: "Schedule", options: [])
+        let category = UNNotificationCategory(identifier: "myCategory", actions: [action, act], intentIdentifiers: [], options: [])
         UNUserNotificationCenter.current().setNotificationCategories([category])
         
         return true
     }
     
-    func scheduleNotification(at date: Date) {
+    func scheduleNotification(at date: Date, showName: String, imageURL: String) {
         UNUserNotificationCenter.current().delegate = self
         
         let calendar = Calendar(identifier: .gregorian)
@@ -41,20 +45,76 @@ import UserNotifications
         content.sound = UNNotificationSound.default()
         content.categoryIdentifier = "myCategory"
         
-        if let path = Bundle.main.path(forResource: "logo", ofType: "png") {
+        
+        if let url = URL(string: imageURL), let data = NSData(contentsOf: url) {
+            if let att = UNNotificationAttachment.create(imageFileIdentifier: showName, data: data as Data, options: nil, baseURL: imageURL) {
+                content.attachments = [att]
+            } else {
+                print("The attachment was not loaded")
+            }
+        } else {
+            print("The attachment was not created")
+        }
+        
+        /*let url = URL(string: imageURL)!
+        do {
+            let attachment = try UNNotificationAttachment(identifier: "logo", url: url, options: nil)
+            content.attachments = [attachment]
+        } catch {
+            print("The attachment was not loaded")
+        }*/
+        
+        let request = UNNotificationRequest(identifier: showName, content: content, trigger: trigger)
+        
+        // UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
+            if let error = error {
+                print("ERROR : \(error)")
+            }
+        })
+    }
+    
+    func scheduleNotificationEveryWeek(showName: String, attachment: String) {
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 60, repeats: true)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Scheduled notification"
+        content.body = "This is your scheduled notification"
+        content.sound = UNNotificationSound.default()
+        
+        if let url = URL(string: attachment), let data = NSData(contentsOf: url) {
+            if let att = UNNotificationAttachment.create(imageFileIdentifier: showName, data: data as Data, options: nil, baseURL: attachment) {
+                content.attachments = [att]
+            } else {
+                print("The attachment was not loaded")
+            }
+        } else {
+            print("The attachment was not created")
+        }
+        
+        /*if let path = Bundle.main.path(forResource: "logo", ofType: "png") {
             let url = URL(fileURLWithPath: path)
+            print(url.absoluteString)
             
-            do {
+            /*do {
                 let attachment = try UNNotificationAttachment(identifier: "logo", url: url, options: nil)
                 content.attachments = [attachment]
             } catch {
                 print("The attachment was not loaded")
-            }
+            }*/
         }
         
-        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        let url = URL(fileURLWithPath: attachment)
         
-        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        do {
+            let attachment = try UNNotificationAttachment(identifier: "logo", url: url, options: nil)
+            content.attachments = [attachment]
+        } catch {
+            print("The attachment was not loaded")
+        }*/
+        
+        let request = UNNotificationRequest(identifier: "Scheduled \(showName)", content: content, trigger: trigger)
+        
         UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
             if let error = error {
                 print("ERROR : \(error)")
@@ -65,9 +125,47 @@ import UserNotifications
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print("Test delegate")
+        
+        let attachment = response.notification.request.content.attachments.first
+        
         if response.actionIdentifier == "remindLater" {
             let newDate = Date(timeInterval: 60, since: Date())
-            scheduleNotification(at: newDate)
+            
+            scheduleNotification(at: newDate, showName: response.notification.request.identifier, imageURL: attachment!.url.absoluteString)
+        } else if response.actionIdentifier == "schedule" {
+            print("Schedule")
+            print(response.notification.request.identifier)
+            print(attachment!.identifier)
+            self.scheduleNotificationEveryWeek(showName: response.notification.request.identifier, attachment: attachment!.identifier)
         }
+        
+        completionHandler()
+    }
+}
+
+extension UNNotificationAttachment {
+    /// Save the image to disk
+    static func create(imageFileIdentifier: String, data: Data, options: [NSObject : AnyObject]?, baseURL: String) -> UNNotificationAttachment? {
+        let fileManager = FileManager.default
+        let tmpSubFolderName = ProcessInfo.processInfo.globallyUniqueString
+        let tmpSubFolderURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(tmpSubFolderName, isDirectory: true)
+        
+        do {
+            try fileManager.createDirectory(at: tmpSubFolderURL, withIntermediateDirectories: true, attributes: nil)
+            
+            let fileURL = tmpSubFolderURL.appendingPathComponent("\(imageFileIdentifier).png")
+            
+            try data.write(to: fileURL)
+            
+            let imageAttachment = try UNNotificationAttachment.init(identifier: baseURL, url: fileURL, options: options)
+            
+            return imageAttachment
+        } catch let error {
+            print(error)
+        }
+        
+        return nil
     }
 }
--- NotificationsUI/ViewController.swift
+++ NotificationsUI/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
     @IBAction func datePickerDidSelectNewDate(_ sender: UIDatePicker) {
         let selectedDate = sender.date
         let delegate = UIApplication.shared.delegate as? AppDelegate
-        delegate?.scheduleNotification(at: selectedDate)
+        delegate?.scheduleNotification(at: selectedDate, showName: "Test", imageURL: "https://www.appcoda.com/wp-content/uploads/2016/10/user-notification-image-1024x599.png")
+    }
+    
+    @IBAction func stopScheduler(_ sender: Any) {
+        let identifier = "Scheduled Test"
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: [identifier])
     }
 }
